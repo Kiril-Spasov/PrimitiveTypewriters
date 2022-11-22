52ed6199bddf484c4503a3dcd22fd7f036b49b5c
Kiril Spasov <spasov.kiril@gmail.com>
2022-11-22 10:24:26 +0200

    Add project files.

---
+++ PrimitiveTypewriters/FormTypewriter.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace PrimitiveTypewriters
+{
+    partial class FormTypewriter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnCheckCost = new System.Windows.Forms.Button();
+            this.TxtResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // BtnCheckCost
+            // 
+            this.BtnCheckCost.Location = new System.Drawing.Point(50, 67);
+            this.BtnCheckCost.Name = "BtnCheckCost";
+            this.BtnCheckCost.Size = new System.Drawing.Size(191, 62);
+            this.BtnCheckCost.TabIndex = 0;
+            this.BtnCheckCost.Text = "Check Cost";
+            this.BtnCheckCost.UseVisualStyleBackColor = true;
+            this.BtnCheckCost.Click += new System.EventHandler(this.BtnCheckCost_Click);
+            // 
+            // TxtResult
+            // 
+            this.TxtResult.Location = new System.Drawing.Point(325, 67);
+            this.TxtResult.Multiline = true;
+            this.TxtResult.Name = "TxtResult";
+            this.TxtResult.Size = new System.Drawing.Size(368, 334);
+            this.TxtResult.TabIndex = 1;
+            // 
+            // FormTypewriter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.TxtResult);
+            this.Controls.Add(this.BtnCheckCost);
+            this.Name = "FormTypewriter";
+            this.Text = "Typewriter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnCheckCost;
+        private System.Windows.Forms.TextBox TxtResult;
+    }
+}
+
---
+++ PrimitiveTypewriters/FormTypewriter.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace PrimitiveTypewriters
+{
+    public partial class FormTypewriter : Form
+    {
+        public FormTypewriter()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnCheckCost_Click(object sender, EventArgs e)
+        {
+            string letters1 = "qwertyuiopasdfghjklzxcvbnm";
+
+            int currentPos;
+            int newPos;
+            int totalCost;
+
+            string line = "";
+            string letter;
+
+            string path = Application.StartupPath + @"\typewriter.txt";
+            StreamReader streamReader = new StreamReader(path);
+
+            bool finished = false;
+
+            while (!finished)
+            {
+                line = streamReader.ReadLine();
+
+                if (line == null)
+                {
+                    finished = true;
+                }
+                else
+                {
+                    totalCost = 0;
+                    currentPos = 0;
+                    newPos = 0;
+
+                    for (int i = 0; i < line.Length; i++)
+                    {
+                        letter = line.Substring(i, 1);
+                        newPos = letters1.IndexOf(letter);
+
+                        if (newPos > currentPos)
+                        {
+                            totalCost += newPos - currentPos;
+                        }
+                        else if (newPos < currentPos)
+                        {
+                            totalCost += currentPos - newPos;
+                        }
+
+                        currentPos = newPos;
+                    }
+
+                    TxtResult.Text += totalCost + Environment.NewLine;        
+                }
+            }
+        }
+    }
+}
